8ce734d6ba03b3d7490947c297400eceb66d99e6
Android Dev <androidedu@google.com>
2023-07-21 09:10:59 -0700

    Starter Groovy to Kotlin DSL (#45)

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -20,9 +20,38 @@ package com.example.inventory.data
 /**
  * Entity data class represents a single row in the database.
  */
-class Item(
-    val id: Int = 0,
+import androidx.room.Entity
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+@Entity(tableName = "items")
+data class Item(
+    val id: Int,
     val name: String,
     val price: Double,
     val quantity: Int
 )
+
+
+@Dao
+interface ItemDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(item: Item)
+
+    @Update
+    suspend fun update(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    @Query("SELECT * from items WHERE id = :id")
+    fun getItem(id: Int): Flow<Item>
+
+    @Query("SELECT * from items ORDER BY name ASC")
+    fun getAllItems(): Flow<List<Item>>
+}
+
--- app/src/main/java/com/example/inventory/ui/item/ItemEntryViewModel.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemEntryViewModel.kt
@@ -22,15 +22,10 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.inventory.data.Item
 import java.text.NumberFormat
+import com.example.inventory.data.ItemsRepository
 
-/**
- * View Model to validate and insert items in the Room database.
- */
-class ItemEntryViewModel : ViewModel() {
+class ItemEntryViewModel(private val itemsRepository: ItemsRepository) : ViewModel() {
 
-    /**
-     * Holds current item ui state
-     */
     var itemUiState by mutableStateOf(ItemUiState())
         private set
 
